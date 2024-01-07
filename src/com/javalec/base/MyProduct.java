97070538e2ae3cb0e4b5e9416728dbe0199a429b
kimsor1 <imoiksr@gmail.com>
2024-01-07 21:00:27 +0900

    MyPage 페이지들 생성

---
+++ src/com/javalec/base/MyProduct.java
@@ -0,0 +1,126 @@
+package com.javalec.base;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JList;
+import javax.swing.JTextField;
+import javax.swing.JCheckBox;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class MyProduct extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JButton btnHome;
+	private JButton btnMy;
+	private JButton btnAlarm;
+	private JButton btnChat;
+	private JButton btnWrite;
+	private JButton btnBack;
+	private JLabel lblImage;
+	private JScrollPane scrollPane;
+	private JTable innertable;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MyProduct frame = new MyProduct();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MyProduct() {
+		setBounds(100, 100, 430, 732);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		getContentPane().add(getBtnHome());
+		getContentPane().add(getBtnMy());
+		getContentPane().add(getBtnAlarm());
+		getContentPane().add(getBtnChat());
+		getContentPane().add(getBtnWrite());
+		getContentPane().add(getBtnBack());
+		getContentPane().add(getScrollPane());
+		getContentPane().add(getLblImage());
+
+	}
+	private JButton getBtnHome() {
+		if (btnHome == null) {
+			btnHome = new JButton("홈");
+			btnHome.setBounds(20, 55, 70, 34);
+		}
+		return btnHome;
+	}
+	private JButton getBtnMy() {
+		if (btnMy == null) {
+			btnMy = new JButton("개인");
+			btnMy.setBounds(100, 55, 70, 34);
+		}
+		return btnMy;
+	}
+	private JButton getBtnAlarm() {
+		if (btnAlarm == null) {
+			btnAlarm = new JButton("알림");
+			btnAlarm.setBounds(180, 55, 70, 34);
+		}
+		return btnAlarm;
+	}
+	private JButton getBtnChat() {
+		if (btnChat == null) {
+			btnChat = new JButton("채팅");
+			btnChat.setBounds(260, 55, 70, 34);
+		}
+		return btnChat;
+	}
+	private JButton getBtnWrite() {
+		if (btnWrite == null) {
+			btnWrite = new JButton("글쓰기");
+			btnWrite.setBounds(340, 55, 70, 34);
+		}
+		return btnWrite;
+	}
+	private JButton getBtnBack() {
+		if (btnBack == null) {
+			btnBack = new JButton("뒤로가기");
+			btnBack.setBounds(150, 650, 117, 29);
+		}
+		return btnBack;
+	}
+	private JLabel getLblImage() {
+		if (lblImage == null) {
+			lblImage = new JLabel("");
+			lblImage.setIcon(new ImageIcon(MyProduct.class.getResource("/com/javalec/images/mainFrame.png")));
+			lblImage.setBounds(0, 0, 430, 704);
+		}
+		return lblImage;
+	}
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setBounds(20, 150, 380, 450);
+			scrollPane.setViewportView(getInnertable());
+		}
+		return scrollPane;
+	}
+	private JTable getInnertable() {
+		if (innertable == null) {
+			innertable = new JTable();
+		}
+		return innertable;
+	}
+}
