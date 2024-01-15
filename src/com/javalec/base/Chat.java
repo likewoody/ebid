2c12f808238825dda610c922b531959d271e8171
kimsor1 <imoiksr@gmail.com>
2024-01-15 10:37:34 +0900

    글쓰기로 이동하게 수정

--- src/com/javalec/base/Chat.java
+++ src/com/javalec/base/Chat.java
@@ -168,7 +168,7 @@ public class Chat extends JDialog {
 			btnWrite = new JButton("글쓰기");
 			btnWrite.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-					Write wrt = new Write();
+					Writing wrt = new Writing();
 					wrt.setVisible(true);
 					dispose();
 				}
