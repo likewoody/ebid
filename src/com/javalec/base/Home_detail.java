30e6dfc7a9290bc9f716d0d938b0804a82135be5
kimsor1 <imoiksr@gmail.com>
2024-01-15 10:36:44 +0900

    글쓰기로 넘어가게 수정

--- src/com/javalec/base/Home_detail.java
+++ src/com/javalec/base/Home_detail.java
@@ -177,7 +177,7 @@ public class Home_detail extends JDialog {
 			btnWrite = new JButton("글쓰기");
 			btnWrite.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-					Write wrt = new Write();
+					Writing wrt = new Writing();
 					wrt.setVisible(true);
 					dispose();
 				}
